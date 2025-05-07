a54672fe2bd7a49fa81c95c8c34137aac3484a9b
qminh011002 <qminh.workmode@gmail.com>
2025-05-07 15:53:51 +0700

    ⚙️  Modify the App Creator

--- app/main.py
+++ app/main.py
@@ -1,26 +1,42 @@
 from fastapi import FastAPI, status
-from apis.v1.routes import routers as v1_routers
-from utils.config import config
+from api.v1.routes import routers as v1_routers
+from core.config import config
 from starlette.middleware.cors import CORSMiddleware
-from utils.class_object import singleton
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from starlette.requests import Request
-from utils.database import db
+from core.database import db
+import logging
+
+# Configure logging
+logger = logging.getLogger("uvicorn")
 
 
-@singleton
 class AppCreator:
     def __init__(self):
+        logger.info(f"[APP]:: Initializing application in {config.ENV} environment")
+
         self.app = FastAPI(
             title=config.PROJECT_NAME,
             description="",
             version=config.PROJECT_VERSION,
             docs_url="/documentation",
+            openapi_tags=[
+                {
+                    "name": "Authentication",
+                    "description": "Endpoints for login, logout, and token management",
+                },
+                {"name": "Users", "description": "User profile and account management"},
+                {
+                    "name": "Roles",
+                    "description": "Endpoints for managing user roles, including creation, updating, deletion, and role assignment.",
+                },
+            ],
         )
 
         if not db.test_connection():
+            logger.error("[APP]:: Failed to connect to the database")
             raise RuntimeError("❌ Failed to connect to the database.")
 
         if config.BACKEND_CORS_ORIGINS:
@@ -31,6 +47,9 @@ class AppCreator:
                 allow_methods=["*"],
                 allow_headers=["*"],
             )
+            logger.info(
+                f"[APP]:: CORS configured with origins: {config.BACKEND_CORS_ORIGINS}"
+            )
 
         # Check health route
         @self.app.get("/", status_code=status.HTTP_200_OK)
@@ -39,12 +58,14 @@ class AppCreator:
 
         # Include route
         self.app.include_router(v1_routers, prefix=config.API_V1)
+        logger.info(f"[APP]:: API routes mounted at {config.API_V1}")
 
         # Error handler
         @self.app.exception_handler(RequestValidationError)
         async def validation_exception_handler(
             request: Request, exc: RequestValidationError
         ):
+            logger.error(f"[APP]:: Validation error: {exc.errors()}")
             return JSONResponse(
                 status_code=422,
                 content={
