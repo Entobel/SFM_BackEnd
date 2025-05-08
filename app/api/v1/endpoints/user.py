698d6b4a05af5b7b56ed2363e9b45775c001d423
qminh011002 <qminh.workmode@gmail.com>
2025-05-08 17:13:02 +0700

    get me route

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter, status
+from api.v1.deps import token_dependency, get_me_usecase_dependency
 
-router = APIRouter(prefix="/users", tags=["User"])
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.get("/me")
+async def get_me(token: token_dependency, get_me_usecase: get_me_usecase_dependency):
+    return get_me_usecase.execute(token=token)
