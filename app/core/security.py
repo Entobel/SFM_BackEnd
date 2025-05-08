98f63b734d1bd74653378da9f3246cf94aad9f56
qminh011002 <qminh.workmode@gmail.com>
2025-05-08 15:08:47 +0700

    function for token

---
+++ app/core/security.py
@@ -0,0 +1,34 @@
+from core.config import config
+from datetime import datetime, timezone
+from schemas.security import TokenRequest
+from jose import jwt
+from passlib.context import CryptContext
+
+
+def create_access_token(token_request: TokenRequest) -> str:
+
+    payload = {
+        "sub": token_request.model_dump(exclude={"expires_delta"}),
+        "exp": datetime.now(timezone.utc) + token_request.expires_delta,
+    }
+
+    return jwt.encode(
+        payload,
+        key=config.security.SECRET_KEY,
+        algorithm=config.security.ALGORITHM,
+    )
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"])
+
+
+def hash_password(password: str) -> str:
+    hashed_password = bcrypt_context.hash(password)
+
+    return hashed_password
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    is_valid = bcrypt_context.verify(password, hashed_password)
+
+    return is_valid
