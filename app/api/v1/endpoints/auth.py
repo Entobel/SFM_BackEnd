b2d9283915ee43cd6a948d9d737c09bab0d7abca
qminh011002 <qminh.workmode@gmail.com>
2025-05-08 15:03:35 +0700

    Clean Architecture

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,35 +1,19 @@
 from domain.services.auth_service import AuthService
 from fastapi import APIRouter, status, Depends
-from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
-from core.database import db
-
 from infrastructure.database.repositories.auth_repository import AuthRepository
-
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from api.v1.deps import login_usecase_dependency
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
-# Login route
-security_dependency = Annotated[OAuth2PasswordRequestForm, Depends()]
-
-
-def get_auth_repository(db: Annotated[Session, Depends(db.get_db)]) -> AuthRepository:
-    return AuthRepository(session=db)
-
-
-def get_auth_service(
-    repo: Annotated[AuthRepository, Depends(get_auth_repository)],
-) -> AuthService:
-    return AuthService(auth_repository=repo)
-
-
-service_dependency = Annotated[AuthService, Depends(get_auth_service)]
-
 
 @router.post("/login")
-async def login(form_data: security_dependency, service: service_dependency):
-    user_name = form_data.username
-    password = form_data.password
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    login_use_case: login_usecase_dependency,
+):
+    _user_name = form_data.username
+    _password = form_data.password
 
-    service.login(username=user_name, password=password)
+    return login_use_case.execute(user_name=_user_name, password=_password)
