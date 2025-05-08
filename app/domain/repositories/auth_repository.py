cdbc87358eef4312c68e7ab85f598bb5d785826b
qminh011002 <qminh.workmode@gmail.com>
2025-05-08 15:09:25 +0700

    modify logic interface for get user by both email and password

--- app/domain/repositories/auth_repository.py
+++ app/domain/repositories/auth_repository.py
@@ -4,9 +4,5 @@ from abc import ABC, abstractmethod
 class IAuthRepository(ABC):
 
     @abstractmethod
-    def get_user_by_phone(self, phone: str):
-        pass
-
-    @abstractmethod
-    def get_user_by_email(self, email: str):
+    def get_user_by_email_or_phone(self, username: str):
         pass
