ea994a3cbad331a8aefa2b4eebce132909bb99d8
qminh011002 <qminh.workmode@gmail.com>
2025-05-23 10:40:28 +0700

    update dependenencies injecttion grow

---
+++ app/presentation/api/v1/dependencies/grow_dependencies.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+
+from fastapi import Depends
+from application.use_cases.growing.list_growing_uc_impl import ListGrowingUC
+from application.interfaces.use_cases.growing.list_growing_uc import IListGrowingUC
+from domain.interfaces.repositories.growing_repository import IGrowingRepository
+from infrastructure.database.repositories.growing_repository_impl import (
+    GrowingRepository,
+)
+from presentation.api.v1.dependencies.common_dependencies import (
+    DatabaseDep,
+    QueryHelperDep,
+)
+
+
+def get_grow_repository(
+    db: DatabaseDep, query_helper: QueryHelperDep
+) -> IGrowingRepository:
+    return GrowingRepository(conn=db, query_helper=query_helper)
+
+
+def get_list_growing_uc(
+    grow_repository: Annotated[IGrowingRepository, Depends(get_grow_repository)],
+) -> IListGrowingUC:
+    return ListGrowingUC(growing_repository=grow_repository)
+
+
+ListGrowingUCDep = Annotated[IListGrowingUC, Depends(get_list_growing_uc)]
