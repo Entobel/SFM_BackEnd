1af51a5f1af7f20f48ef5bc2d549f340e56237d9
qminh011002 <qminh.workmode@gmail.com>
2025-05-23 16:26:53 +0700

    update shift repo for new features

--- app/domain/interfaces/repositories/shift_repository.py
+++ app/domain/interfaces/repositories/shift_repository.py
@@ -4,4 +4,16 @@ from abc import ABC, abstractmethod
 
 class IShiftRepository(ABC):
     @abstractmethod
+    def get_shift_by_id(self, id: int) -> ShiftEntity: ...
+
+    @abstractmethod
+    def get_shift_by_name(self, name: str) -> ShiftEntity: ...
+
+    @abstractmethod
+    def update_status_shift(self, shift_entity: ShiftEntity) -> bool: ...
+
+    @abstractmethod
+    def update_shift(self, shift_entity: ShiftEntity) -> bool: ...
+
+    @abstractmethod
     def get_all_shifts(self) -> list[ShiftEntity]: ...
