f1f37b3154ae39afd9a82273ce4777c3cb46bf80
qminh011002 <qminh.workmode@gmail.com>
2025-05-23 16:27:06 +0700

    update shift repo

--- app/infrastructure/database/repositories/shift_repository_impl.py
+++ app/infrastructure/database/repositories/shift_repository_impl.py
@@ -9,6 +9,58 @@ class ShiftRepository(IShiftRepository):
     def __init__(self, conn: psycopg2.extensions.connection):
         self.conn = conn
 
+    def get_shift_by_id(self, id: int) -> ShiftEntity:
+        query = """
+            SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shift WHERE id = %s
+        """
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(query, (id,))
+            return ShiftEntity.from_row(cur.fetchone())
+
+    def get_shift_by_name(self, name: str) -> ShiftEntity:
+
+        query = """
+            SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shift WHERE name = %s
+        """
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(query, (name,))
+            return ShiftEntity.from_row(cur.fetchone())
+
+    def update_status_shift(self, shift_entity: ShiftEntity) -> bool:
+        query = """
+            UPDATE shift SET is_active = %s WHERE id = %s
+        """
+        shift_id = shift_entity.id
+        shift_is_active = shift_entity.is_active
+
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(query, (shift_is_active, shift_id))
+
+            if cur.rowcount > 0:
+                self.conn.commit()
+                return True
+            else:
+                self.conn.rollback()
+                return False
+
+    def update_shift(self, shift_entity: ShiftEntity) -> bool:
+        query = """
+            UPDATE shift SET name = %s, description = %s WHERE id = %s
+        """
+        shift_id = shift_entity.id
+        shift_name = shift_entity.name
+        shift_description = shift_entity.description
+
+        with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(query, (shift_name, shift_description, shift_id))
+
+            if cur.rowcount > 0:
+                self.conn.commit()
+                return True
+            else:
+                self.conn.rollback()
+                return False
+
     def get_all_shifts(self) -> list[ShiftEntity]:
         query = """
             SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shift WHERE is_active = TRUE
