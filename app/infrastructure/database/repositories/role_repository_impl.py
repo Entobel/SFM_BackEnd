b274d4cd6c9f71b0c0a5baa5c6aedf294c5c465d
qminh011002 <qminh.workmode@gmail.com>
2025-05-27 10:48:48 +0700

    role_repository_impl.py

--- app/infrastructure/database/repositories/role_repository_impl.py
+++ app/infrastructure/database/repositories/role_repository_impl.py
@@ -2,8 +2,8 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 
 from domain.entities.role_entity import RoleEntity
-from domain.interfaces.services.query_helper_service import IQueryHelperService
 from domain.interfaces.repositories.role_repository import IRoleRepository
+from domain.interfaces.services.query_helper_service import IQueryHelperService
 
 
 class RoleRepository(IRoleRepository):
