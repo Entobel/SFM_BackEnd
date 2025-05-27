2e2aa28f80d29fedf4ed18bd1050aa8606ae84d6
qminh011002 <qminh.workmode@gmail.com>
2025-05-27 10:48:49 +0700

    diet_dependencies.py

--- app/presentation/api/v1/dependencies/diet_dependencies.py
+++ app/presentation/api/v1/dependencies/diet_dependencies.py
@@ -1,22 +1,22 @@
 from typing import Annotated
 
 from fastapi import Depends
-from application.interfaces.use_cases.diet.update_diet_uc import IUpdateDietUC
-from application.use_cases.diet.update_diet_uc_impl import UpdateDietUC
-from application.use_cases.diet.list_diet_uc_impl import ListDietUC
-from application.interfaces.use_cases.diet.list_diet_uc import IListDietUC
+
 from application.interfaces.use_cases.diet.create_diet_uc import ICreateDietUC
+from application.interfaces.use_cases.diet.list_diet_uc import IListDietUC
+from application.interfaces.use_cases.diet.update_diet_uc import IUpdateDietUC
+from application.interfaces.use_cases.diet.update_status_diet_uc import \
+    IUpdateStatusDietUC
 from application.use_cases.diet.create_diet_uc_impl import CreateDietUC
-from application.use_cases.diet.update_status_diet_uc_impl import UpdateStatusDietUC
-from application.interfaces.use_cases.diet.update_status_diet_uc import (
-    IUpdateStatusDietUC,
-)
-from infrastructure.database.repositories.diet_repository_impl import DietRepository
+from application.use_cases.diet.list_diet_uc_impl import ListDietUC
+from application.use_cases.diet.update_diet_uc_impl import UpdateDietUC
+from application.use_cases.diet.update_status_diet_uc_impl import \
+    UpdateStatusDietUC
 from domain.interfaces.repositories.diet_repository import IDietRepository
+from infrastructure.database.repositories.diet_repository_impl import \
+    DietRepository
 from presentation.api.v1.dependencies.common_dependencies import (
-    DatabaseDep,
-    QueryHelperDep,
-)
+    DatabaseDep, QueryHelperDep)
 
 
 def get_diet_repository(
