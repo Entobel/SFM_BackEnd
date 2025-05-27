aaa6b9f433c805188769da4fbc779cfa960936e0
qminh011002 <qminh.workmode@gmail.com>
2025-05-27 10:48:49 +0700

    common_dependencies.py

--- app/presentation/api/v1/dependencies/common_dependencies.py
+++ app/presentation/api/v1/dependencies/common_dependencies.py
@@ -1,22 +1,23 @@
+from typing import Annotated
+
 import psycopg2.extensions
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
-from domain.interfaces.services.query_helper_service import IQueryHelperService
-from infrastructure.services.query_helper_service_impl import QueryHelper
-from infrastructure.services.password_service_imply import PasswordService
-from infrastructure.services.token_service_imply import TokenService
-from infrastructure.services.access_policy_service_impl import AccessPolicyService
-from infrastructure.database.repositories.user_repository_impl import UserRepository
-
-from domain.interfaces.services.password_service import IPasswordService
-from domain.interfaces.repositories.user_repository import IUserRepository
-from domain.interfaces.services.token_service import ITokenService
-from domain.interfaces.services.token_service import ITokenService
-from domain.interfaces.services.access_policy_service import IAccessPolicyService
-
 from core.database import db
-from fastapi import Depends
-from typing import Annotated
+from domain.interfaces.repositories.user_repository import IUserRepository
+from domain.interfaces.services.access_policy_service import \
+    IAccessPolicyService
+from domain.interfaces.services.password_service import IPasswordService
+from domain.interfaces.services.query_helper_service import IQueryHelperService
+from domain.interfaces.services.token_service import ITokenService
+from infrastructure.database.repositories.user_repository_impl import \
+    UserRepository
+from infrastructure.services.access_policy_service_impl import \
+    AccessPolicyService
+from infrastructure.services.password_service_imply import PasswordService
+from infrastructure.services.query_helper_service_impl import QueryHelper
+from infrastructure.services.token_service_imply import TokenService
 
 DatabaseDep = Annotated[psycopg2.extensions.connection, Depends(db.get_db)]
 
