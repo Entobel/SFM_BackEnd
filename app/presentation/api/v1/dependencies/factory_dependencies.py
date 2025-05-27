06ebc70489a7f0207e3fc78f7de2309c5d24268f
qminh011002 <qminh.workmode@gmail.com>
2025-05-27 10:48:49 +0700

    factory_dependencies.py

--- app/presentation/api/v1/dependencies/factory_dependencies.py
+++ app/presentation/api/v1/dependencies/factory_dependencies.py
@@ -1,28 +1,29 @@
 from typing import Annotated
 
 from fastapi import Depends
-from application.use_cases.factory.update_status_factory_uc_impl import (
-    UpdateStatusFactoryUC,
-)
-from application.interfaces.use_cases.factory.update_status_factory_uc import (
-    IUpdateStatusFactoryUC,
-)
-from application.interfaces.use_cases.factory.update_factory_uc import (
-    IUpdateFactoryUC,
-)
-from application.use_cases.factory.update_factory_uc_impl import UpdateFactoryUC
-from application.use_cases.factory.create_factory_uc_impl import CreateFactoryUC
-from application.interfaces.use_cases.factory.create_factory_uc import ICreateFactoryUC
+
+from application.interfaces.use_cases.factory.create_factory_uc import \
+    ICreateFactoryUC
+from application.interfaces.use_cases.factory.list_factory_uc import \
+    IListFactoryUC
+from application.interfaces.use_cases.factory.update_factory_uc import \
+    IUpdateFactoryUC
+from application.interfaces.use_cases.factory.update_status_factory_uc import \
+    IUpdateStatusFactoryUC
+from application.use_cases.factory.create_factory_uc_impl import \
+    CreateFactoryUC
+from application.use_cases.factory.list_factory_uc_impl import \
+    ListFactoryUCImpl
+from application.use_cases.factory.update_factory_uc_impl import \
+    UpdateFactoryUC
+from application.use_cases.factory.update_status_factory_uc_impl import \
+    UpdateStatusFactoryUC
+from domain.interfaces.repositories.factory_repository import \
+    IFactoryRepository
+from infrastructure.database.repositories.factory_repository_impl import \
+    FactoryRepository
 from presentation.api.v1.dependencies.common_dependencies import (
-    DatabaseDep,
-    QueryHelperDep,
-)
-from infrastructure.database.repositories.factory_repository_impl import (
-    FactoryRepository,
-)
-from domain.interfaces.repositories.factory_repository import IFactoryRepository
-from application.interfaces.use_cases.factory.list_factory_uc import IListFactoryUC
-from application.use_cases.factory.list_factory_uc_impl import ListFactoryUCImpl
+    DatabaseDep, QueryHelperDep)
 
 
 def get_factory_repository(
@@ -31,26 +32,29 @@ def get_factory_repository(
     return FactoryRepository(conn=db, query_helper=query_helper)
 
 
+FactoryRepositoryDep = Annotated[IFactoryRepository, Depends(get_factory_repository)]
+
+
 def get_list_factory_use_case(
-    repo: Annotated[IFactoryRepository, Depends(get_factory_repository)],
+    repo: FactoryRepositoryDep,
 ) -> IListFactoryUC:
     return ListFactoryUCImpl(factory_repository=repo)
 
 
 def get_create_factory_use_case(
-    repo: Annotated[IFactoryRepository, Depends(get_factory_repository)],
+    repo: FactoryRepositoryDep,
 ) -> ICreateFactoryUC:
     return CreateFactoryUC(factory_repository=repo)
 
 
 def get_update_factory_use_case(
-    repo: Annotated[IFactoryRepository, Depends(get_factory_repository)],
+    repo: FactoryRepositoryDep,
 ) -> IUpdateFactoryUC:
     return UpdateFactoryUC(factory_repository=repo)
 
 
 def get_update_status_factory_use_case(
-    repo: Annotated[IFactoryRepository, Depends(get_factory_repository)],
+    repo: FactoryRepositoryDep,
 ) -> IUpdateStatusFactoryUC:
     return UpdateStatusFactoryUC(factory_repository=repo)
 
