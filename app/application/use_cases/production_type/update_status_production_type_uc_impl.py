4face417368b3b643a83d78589f3253a85553659
qminh011002 <qminh.workmode@gmail.com>
2025-05-30 16:12:05 +0700

    update_status_production_type_uc_impl.py

--- app/application/use_cases/production_type/update_status_production_type_uc_impl.py
+++ app/application/use_cases/production_type/update_status_production_type_uc_impl.py
@@ -1,6 +1,6 @@
+from application.dto.produciton_type_dto import ProductionTypeDTO
 from application.interfaces.use_cases.production_type.update_status_production_type_uc import \
     IUpdateStatusProductionTypeUC
-from application.schemas.produciton_type_dto import ProductionTypeDTO
 from core.exception import BadRequestError
 from domain.interfaces.repositories.production_type_repository import \
     IProductionTypeRepository
