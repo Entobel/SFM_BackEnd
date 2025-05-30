e50d1b2069b8a00c91c0778169d1cb24d887d8fb
qminh011002 <qminh.workmode@gmail.com>
2025-05-30 16:12:05 +0700

    update_production_type_uc_impl.py

--- app/application/use_cases/production_type/update_production_type_uc_impl.py
+++ app/application/use_cases/production_type/update_production_type_uc_impl.py
@@ -1,6 +1,6 @@
+from application.dto.produciton_type_dto import ProductionTypeDTO
 from application.interfaces.use_cases.production_type.update_production_type_uc import \
     IUpdateProductionTypeUC
-from application.schemas.produciton_type_dto import ProductionTypeDTO
 from core.exception import BadRequestError
 from domain.interfaces.repositories.production_type_repository import \
     IProductionTypeRepository
