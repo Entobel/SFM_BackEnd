54b2b64c6ec001328cd82253682be97b01e74123
qminh011002 <qminh.workmode@gmail.com>
2025-05-30 16:12:06 +0700

    create_shift_uc_impl.py

--- app/application/use_cases/shift/create_shift_uc_impl.py
+++ app/application/use_cases/shift/create_shift_uc_impl.py
@@ -1,6 +1,6 @@
+from application.dto.shift_dto import ShiftDTO
 from application.interfaces.use_cases.shift.create_shift_uc import \
     ICreateShiftUC
-from application.schemas.shift_dto import ShiftDTO
 from core.exception import BadRequestError
 from domain.entities.shift_entity import ShiftEntity
 from domain.interfaces.repositories.shift_repository import IShiftRepository
@@ -11,13 +11,13 @@ class CreateShiftUC(ICreateShiftUC):
         self.shift_repository = shift_repository
 
     def execute(self, shift_dto: ShiftDTO) -> bool:
-        if self.shift_repository.get_shift_by_name(shift_dto.name):
-            raise BadRequestError("ETB-shift_da_ton_tai")
-
         shift_entity = ShiftEntity(
             name=shift_dto.name,
             description=shift_dto.description,
         )
+
+        if self.shift_repository.get_shift_by_name(shift_entity=shift_entity):
+            raise BadRequestError("ETB-shift_da_ton_tai")
 
         is_success = self.shift_repository.create_shift(shift_entity)
 
