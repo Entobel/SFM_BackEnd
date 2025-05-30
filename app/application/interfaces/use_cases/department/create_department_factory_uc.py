f9a1e4d14ced47a270a90ab3d9a145c9b8b5a83a
qminh011002 <qminh.workmode@gmail.com>
2025-05-30 16:12:04 +0700

    create_department_factory_uc.py

--- app/application/interfaces/use_cases/department/create_department_factory_uc.py
+++ app/application/interfaces/use_cases/department/create_department_factory_uc.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
-from application.schemas.department_dto import DepartmentDTO
-from application.schemas.factory_dto import FactoryDTO
+from application.dto.department_dto import DepartmentDTO
+from application.dto.factory_dto import FactoryDTO
 
 
 class ICreateDepartmentFactoryUC(ABC):
