7eeda07f590034331a0d2f398f8997df89669e7b
qminh011002 <qminh.workmode@gmail.com>
2025-05-30 16:12:05 +0700

    update_status_department_factory_role_uc_impl.py

--- app/application/use_cases/department/update_status_department_factory_role_uc_impl.py
+++ app/application/use_cases/department/update_status_department_factory_role_uc_impl.py
@@ -1,7 +1,7 @@
+from application.dto.department_factory_role_dto import \
+    DepartmentFactoryRoleDTO
 from application.interfaces.use_cases.department.update_status_department_factory_role_uc import \
     IUpdateStatusDepartmentFactoryRoleUC
-from application.schemas.department_factory_role_dto import \
-    DepartmentFactoryRoleDTO
 from core.exception import BadRequestError, NotFoundError
 from domain.interfaces.repositories.deparment_factory_role_repository import \
     IDepartmentFactoryRoleRepository
