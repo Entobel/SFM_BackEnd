4ce3bd371638c14b5d0c89a91222b182772bbde4
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:29 +0700

    production_type_denpendencies.py

--- app/presentation/api/v1/dependencies/production_type_denpendencies.py
+++ app/presentation/api/v1/dependencies/production_type_denpendencies.py
@@ -2,54 +2,71 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from app.application.interfaces.use_cases.production_type.create_production_type_uc import \
-    ICreateProductionTypeUC
-from app.application.interfaces.use_cases.production_type.list_production_type_uc import \
-    IListProductionTypeUC
-from app.application.interfaces.use_cases.production_type.update_production_type_uc import \
-    IUpdateProductionTypeUC
-from app.application.interfaces.use_cases.production_type.update_status_production_type_uc import \
-    IUpdateStatusProductionTypeUC
-from app.application.use_cases.production_type.create_production_type_uc_impl import \
-    CreateProductionTypeUC
-from app.application.use_cases.production_type.list_production_type_uc_impl import \
-    ListProductionTypeUC
-from app.application.use_cases.production_type.update_production_type_uc_impl import \
-    UpdateProductionTypeUC
-from app.application.use_cases.production_type.update_status_production_type_uc_impl import \
-    UpdateStatusProductionTypeUC
-from app.domain.interfaces.repositories.production_type_repository import \
-    IProductionTypeRepository
-from app.infrastructure.database.repositories.production_type_repository_impl import \
-    ProductionTypeRepository
+from app.application.interfaces.use_cases.production_type.create_production_type_uc import (
+    ICreateProductionTypeUC,
+)
+from app.application.interfaces.use_cases.production_type.list_production_type_uc import (
+    IListProductionTypeUC,
+)
+from app.application.interfaces.use_cases.production_type.update_production_type_uc import (
+    IUpdateProductionTypeUC,
+)
+from app.application.interfaces.use_cases.production_type.update_status_production_type_uc import (
+    IUpdateStatusProductionTypeUC,
+)
+from app.application.use_cases.production_type.create_production_type_uc_impl import (
+    CreateProductionTypeUC,
+)
+from app.application.use_cases.production_type.list_production_type_uc_impl import (
+    ListProductionTypeUC,
+)
+from app.application.use_cases.production_type.update_production_type_uc_impl import (
+    UpdateProductionTypeUC,
+)
+from app.application.use_cases.production_type.update_status_production_type_uc_impl import (
+    UpdateStatusProductionTypeUC,
+)
+from app.domain.interfaces.repositories.production_type_repository import (
+    IProductionTypeRepository,
+)
+from app.infrastructure.database.repositories.production_type_repository_impl import (
+    ProductionTypeRepository,
+)
 from app.presentation.api.v1.dependencies.common_dependencies import (
-    DatabaseDep, QueryHelperDep)
+    DatabaseDep,
+    QueryHelperDep,
+)
 
 
 def get_production_type_repository(db: DatabaseDep, query_helper: QueryHelperDep):
     return ProductionTypeRepository(conn=db, query_helper=query_helper)
 
 
+ProductionTypeRepositoryDep = Annotated[
+    IProductionTypeRepository, Depends(get_production_type_repository)
+]
+
+
 def get_production_type_uc(
-    repo: Annotated[IProductionTypeRepository, Depends(get_production_type_repository)],
+    repo: ProductionTypeRepositoryDep,
 ) -> IListProductionTypeUC:
     return ListProductionTypeUC(repo=repo)
 
 
 def get_create_production_type_uc(
-    repo: Annotated[IProductionTypeRepository, Depends(get_production_type_repository)],
+    repo: ProductionTypeRepositoryDep,
 ) -> ICreateProductionTypeUC:
     return CreateProductionTypeUC(repo=repo)
 
 
 def get_update_production_type_uc(
-    repo: Annotated[IProductionTypeRepository, Depends(get_production_type_repository)],
+    repo: ProductionTypeRepositoryDep,
 ) -> IUpdateProductionTypeUC:
     return UpdateProductionTypeUC(repo=repo)
 
 
 def get_update_status_production_type_uc(
-    repo: Annotated[IProductionTypeRepository, Depends(get_production_type_repository)],
+    repo: ProductionTypeRepositoryDep,
 ) -> IUpdateStatusProductionTypeUC:
     return UpdateStatusProductionTypeUC(repo=repo)
 
