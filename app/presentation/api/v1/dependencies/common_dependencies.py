af52b191a3d504c5426f437e8f25250460a06eff
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:29 +0700

    common_dependencies.py

--- app/presentation/api/v1/dependencies/common_dependencies.py
+++ app/presentation/api/v1/dependencies/common_dependencies.py
@@ -5,16 +5,17 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 
 from app.core.database import db
+from app.domain.interfaces.repositories.common_repository import ICommonRepository
 from app.domain.interfaces.repositories.user_repository import IUserRepository
-from app.domain.interfaces.services.access_policy_service import \
-    IAccessPolicyService
+from app.domain.interfaces.services.access_policy_service import IAccessPolicyService
 from app.domain.interfaces.services.password_service import IPasswordService
 from app.domain.interfaces.services.query_helper_service import IQueryHelperService
 from app.domain.interfaces.services.token_service import ITokenService
-from app.infrastructure.database.repositories.user_repository_impl import \
-    UserRepository
-from app.infrastructure.services.access_policy_service_impl import \
-    AccessPolicyService
+from app.infrastructure.database.repositories.common_repository_impl import (
+    CommonRepository,
+)
+from app.infrastructure.database.repositories.user_repository_impl import UserRepository
+from app.infrastructure.services.access_policy_service_impl import AccessPolicyService
 from app.infrastructure.services.password_service_imply import PasswordService
 from app.infrastructure.services.query_helper_service_impl import QueryHelper
 from app.infrastructure.services.token_service_imply import TokenService
@@ -33,10 +34,20 @@ def get_query_helper() -> IQueryHelperService:
 QueryHelperDep = Annotated[IQueryHelperService, Depends(get_query_helper)]
 
 
+def get_common_repository(db: DatabaseDep) -> ICommonRepository:
+    return CommonRepository(conn=db)
+
+
+CommonRepositoryDep = Annotated[ICommonRepository, Depends(get_common_repository)]
+
+
 def get_user_repository(
     db: DatabaseDep, query_helper: QueryHelperDep
 ) -> IUserRepository:
     return UserRepository(conn=db, query_helper=query_helper)
+
+
+UserRepositoryDep = Annotated[IUserRepository, Depends(get_user_repository)]
 
 
 def get_access_policy_service() -> IAccessPolicyService:
