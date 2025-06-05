d5e8f0bf424243add2299d8fa04c518147483b72
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:28 +0700

    update_status_production_object_uc_impl.py

--- app/application/use_cases/production_object/update_status_production_object_uc_impl.py
+++ app/application/use_cases/production_object/update_status_production_object_uc_impl.py
@@ -1,9 +1,12 @@
 from app.application.dto.production_object_dto import ProductionObjectDTO
-from app.application.interfaces.use_cases.production_object.update_status_production_object_uc import \
-    IUpdateStatusProductionObjectUC
+from app.application.interfaces.use_cases.production_object.update_status_production_object_uc import (
+    IUpdateStatusProductionObjectUC,
+)
 from app.core.exception import BadRequestError
-from app.domain.interfaces.repositories.production_object_repository import \
-    IProductionObjectRepository
+from app.domain.entities.production_object_entity import ProductionObjectEntity
+from app.domain.interfaces.repositories.production_object_repository import (
+    IProductionObjectRepository,
+)
 
 
 class UpdateStatusProductionObjectUC(IUpdateStatusProductionObjectUC):
@@ -11,14 +14,16 @@ class UpdateStatusProductionObjectUC(IUpdateStatusProductionObjectUC):
         self.repo = repo
 
     def execute(self, production_object_dto: ProductionObjectDTO) -> bool:
+        query_entity = ProductionObjectEntity(id=production_object_dto.id)
+
         production_object_entity = self.repo.get_production_object_by_id(
-            id=production_object_dto.id
+            production_object_entity=query_entity
         )
 
         if production_object_entity is None:
             raise BadRequestError("ETB-production_object_not_found")
 
-        if production_object_entity.is_active == production_object_dto.is_active:
+        if production_object_entity.is_active == query_entity.is_active:
             return True
 
         production_object_entity.change_is_active(
