34ab1d77c8c105598f18b64c9552f8b4d9773e31
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:29 +0700

    shift_repository_impl.py

--- app/infrastructure/database/repositories/shift_repository_impl.py
+++ app/infrastructure/database/repositories/shift_repository_impl.py
@@ -15,7 +15,7 @@ class ShiftRepository(IShiftRepository):
 
     def get_shift_by_id(self, shift_entity: ShiftEntity) -> ShiftEntity | None:
         query = """
-            SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shift WHERE id = %s
+            SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shifts WHERE id = %s
         """
 
         shift_id = shift_entity.id
@@ -28,7 +28,7 @@ class ShiftRepository(IShiftRepository):
     def get_shift_by_name(self, shift_entity: ShiftEntity) -> ShiftEntity | None:
 
         query = """
-            SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shift WHERE name = %s
+            SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shifts WHERE name = %s
         """
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query, (shift_entity.name,))
@@ -45,17 +45,11 @@ class ShiftRepository(IShiftRepository):
 
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query, (shift_is_active, shift_id))
-
-            if cur.rowcount > 0:
-                self.conn.commit()
-                return True
-            else:
-                self.conn.rollback()
-                return False
+            return cur.rowcount > 0
 
     def create_shift(self, shift_entity: ShiftEntity) -> bool:
         query = """
-            INSERT INTO shift (name, description) VALUES (%s, %s)
+            INSERT INTO shifts (name, description) VALUES (%s, %s)
         """
         shift_name = shift_entity.name
         shift_description = shift_entity.description
@@ -63,16 +57,11 @@ class ShiftRepository(IShiftRepository):
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query, (shift_name, shift_description))
 
-            if cur.rowcount > 0:
-                self.conn.commit()
-                return True
-            else:
-                self.conn.rollback()
-                return False
+            return cur.rowcount > 0
 
     def update_shift(self, shift_entity: ShiftEntity) -> bool:
         query = """
-            UPDATE shift SET name = %s, description = %s WHERE id = %s
+            UPDATE shifts SET name = %s, description = %s WHERE id = %s
         """
         shift_id = shift_entity.id
         shift_name = shift_entity.name
@@ -81,12 +70,7 @@ class ShiftRepository(IShiftRepository):
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query, (shift_name, shift_description, shift_id))
 
-            if cur.rowcount > 0:
-                self.conn.commit()
-                return True
-            else:
-                self.conn.rollback()
-                return False
+            return cur.rowcount > 0
 
     def get_all_shifts(
         self,
@@ -111,7 +95,7 @@ class ShiftRepository(IShiftRepository):
 
         # count
         count_sql = f"""
-        SELECT COUNT(*) FROM shift {qb.where_sql()}
+        SELECT COUNT(*) FROM shifts {qb.where_sql()}
         """
 
         with self.conn.cursor() as cur:
@@ -121,7 +105,7 @@ class ShiftRepository(IShiftRepository):
         # fetch page
         limit_sql, limit_params = qb.paginate(page, page_size)
         data_sql = f"""
-        SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shift {qb.where_sql()} ORDER BY s_id DESC {limit_sql}
+        SELECT id as s_id, name as s_name, description as s_description, is_active as s_is_active FROM shifts {qb.where_sql()} ORDER BY s_id DESC {limit_sql}
         """
 
         params = qb.all_params(limit_params)
