ff1f3499d4931bc609992c994a35ccbbec6cdbc9
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:29 +0700

    diet_dependencies.py

--- app/presentation/api/v1/dependencies/diet_dependencies.py
+++ app/presentation/api/v1/dependencies/diet_dependencies.py
@@ -5,18 +5,19 @@ from fastapi import Depends
 from app.application.interfaces.use_cases.diet.create_diet_uc import ICreateDietUC
 from app.application.interfaces.use_cases.diet.list_diet_uc import IListDietUC
 from app.application.interfaces.use_cases.diet.update_diet_uc import IUpdateDietUC
-from app.application.interfaces.use_cases.diet.update_status_diet_uc import \
-    IUpdateStatusDietUC
+from app.application.interfaces.use_cases.diet.update_status_diet_uc import (
+    IUpdateStatusDietUC,
+)
 from app.application.use_cases.diet.create_diet_uc_impl import CreateDietUC
 from app.application.use_cases.diet.list_diet_uc_impl import ListDietUC
 from app.application.use_cases.diet.update_diet_uc_impl import UpdateDietUC
-from app.application.use_cases.diet.update_status_diet_uc_impl import \
-    UpdateStatusDietUC
+from app.application.use_cases.diet.update_status_diet_uc_impl import UpdateStatusDietUC
 from app.domain.interfaces.repositories.diet_repository import IDietRepository
-from app.infrastructure.database.repositories.diet_repository_impl import \
-    DietRepository
+from app.infrastructure.database.repositories.diet_repository_impl import DietRepository
 from app.presentation.api.v1.dependencies.common_dependencies import (
-    DatabaseDep, QueryHelperDep)
+    DatabaseDep,
+    QueryHelperDep,
+)
 
 
 def get_diet_repository(
@@ -25,26 +26,29 @@ def get_diet_repository(
     return DietRepository(conn=db, query_helper=query_helper)
 
 
+DietRepositoryDep = Annotated[IDietRepository, Depends(get_diet_repository)]
+
+
 def get_list_diet_uc(
-    repository: Annotated[IDietRepository, Depends(get_diet_repository)],
+    repository: DietRepositoryDep,
 ) -> IListDietUC:
     return ListDietUC(diet_repository=repository)
 
 
 def get_create_diet_uc(
-    repository: Annotated[IDietRepository, Depends(get_diet_repository)],
+    repository: DietRepositoryDep,
 ) -> ICreateDietUC:
     return CreateDietUC(diet_repository=repository)
 
 
 def get_update_status_diet_uc(
-    repository: Annotated[IDietRepository, Depends(get_diet_repository)],
+    repository: DietRepositoryDep,
 ) -> IUpdateStatusDietUC:
     return UpdateStatusDietUC(diet_repository=repository)
 
 
 def get_update_diet_uc(
-    repository: Annotated[IDietRepository, Depends(get_diet_repository)],
+    repository: DietRepositoryDep,
 ) -> IUpdateDietUC:
     return UpdateDietUC(diet_repository=repository)
 
