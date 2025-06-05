0de252fa3d69773f2e554401fd3e1b9636106b2f
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:29 +0700

    shift_dependencies.py

--- app/presentation/api/v1/dependencies/shift_dependencies.py
+++ app/presentation/api/v1/dependencies/shift_dependencies.py
@@ -2,23 +2,26 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from app.application.interfaces.use_cases.shift.create_shift_uc import \
-    ICreateShiftUC
+from app.application.interfaces.use_cases.shift.create_shift_uc import ICreateShiftUC
 from app.application.interfaces.use_cases.shift.list_shift_uc import IListShiftUC
-from app.application.interfaces.use_cases.shift.update_shift_uc import \
-    IUpdateShiftUC
-from app.application.interfaces.use_cases.shift.update_status_shift_uc import \
-    IUpdateStatusShiftUC
+from app.application.interfaces.use_cases.shift.update_shift_uc import IUpdateShiftUC
+from app.application.interfaces.use_cases.shift.update_status_shift_uc import (
+    IUpdateStatusShiftUC,
+)
 from app.application.use_cases.shift.create_shift_uc_impl import CreateShiftUC
 from app.application.use_cases.shift.list_shift_uc_impl import ListShiftUC
 from app.application.use_cases.shift.update_shift_uc_impl import UpdateShiftUC
-from app.application.use_cases.shift.update_status_shift_uc_imply import \
-    UpdateStatusShiftUC
+from app.application.use_cases.shift.update_status_shift_uc_imply import (
+    UpdateStatusShiftUC,
+)
 from app.domain.interfaces.repositories.shift_repository import IShiftRepository
-from app.infrastructure.database.repositories.shift_repository_impl import \
-    ShiftRepository
+from app.infrastructure.database.repositories.shift_repository_impl import (
+    ShiftRepository,
+)
 from app.presentation.api.v1.dependencies.common_dependencies import (
-    DatabaseDep, QueryHelperDep)
+    DatabaseDep,
+    QueryHelperDep,
+)
 
 
 def get_shift_repository(
@@ -27,26 +30,29 @@ def get_shift_repository(
     return ShiftRepository(conn=db, query_helper=query_helper)
 
 
+ShiftRepositoryDep = Annotated[IShiftRepository, Depends(get_shift_repository)]
+
+
 def get_list_shift_use_case(
-    shift_repository: Annotated[IShiftRepository, Depends(get_shift_repository)],
+    shift_repository: ShiftRepositoryDep,
 ) -> IListShiftUC:
     return ListShiftUC(shift_repository=shift_repository)
 
 
 def get_create_shift_use_case(
-    shift_repository: Annotated[IShiftRepository, Depends(get_shift_repository)],
+    shift_repository: ShiftRepositoryDep,
 ) -> ICreateShiftUC:
     return CreateShiftUC(shift_repository=shift_repository)
 
 
 def get_update_status_shift_use_case(
-    shift_repository: Annotated[IShiftRepository, Depends(get_shift_repository)],
+    shift_repository: ShiftRepositoryDep,
 ) -> IUpdateStatusShiftUC:
     return UpdateStatusShiftUC(shift_repository=shift_repository)
 
 
 def get_update_shift_use_case(
-    shift_repository: Annotated[IShiftRepository, Depends(get_shift_repository)],
+    shift_repository: ShiftRepositoryDep,
 ) -> IUpdateShiftUC:
     return UpdateShiftUC(shift_repository=shift_repository)
 
