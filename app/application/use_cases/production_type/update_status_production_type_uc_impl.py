86a643b08c76a29f23236078753e25dcc8e56626
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:28 +0700

    update_status_production_type_uc_impl.py

--- app/application/use_cases/production_type/update_status_production_type_uc_impl.py
+++ app/application/use_cases/production_type/update_status_production_type_uc_impl.py
@@ -1,9 +1,12 @@
 from app.application.dto.produciton_type_dto import ProductionTypeDTO
-from app.application.interfaces.use_cases.production_type.update_status_production_type_uc import \
-    IUpdateStatusProductionTypeUC
+from app.application.interfaces.use_cases.production_type.update_status_production_type_uc import (
+    IUpdateStatusProductionTypeUC,
+)
 from app.core.exception import BadRequestError
-from app.domain.interfaces.repositories.production_type_repository import \
-    IProductionTypeRepository
+from app.domain.entities.production_type_entity import ProductionTypeEntity
+from app.domain.interfaces.repositories.production_type_repository import (
+    IProductionTypeRepository,
+)
 
 
 class UpdateStatusProductionTypeUC(IUpdateStatusProductionTypeUC):
@@ -11,14 +14,16 @@ class UpdateStatusProductionTypeUC(IUpdateStatusProductionTypeUC):
         self.repo = repo
 
     def execute(self, production_type_dto: ProductionTypeDTO) -> bool:
+        query_entity = ProductionTypeEntity(id=production_type_dto.id)
+
         production_type_entity = self.repo.get_production_type_by_id(
-            id=production_type_dto.id
+            production_type_entity=query_entity
         )
 
         if production_type_entity is None:
             raise BadRequestError("ETB-loai_san_pham_khong_ton_tai")
 
-        if production_type_entity.is_active == production_type_dto.is_active:
+        if production_type_entity.is_active == query_entity.is_active:
             return True
 
         production_type_entity.change_is_active(is_active=production_type_dto.is_active)
