1db41e2d7bccd28eef761651974e261bc95964b4
qminh011002 <qminh.workmode@gmail.com>
2025-06-05 10:38:28 +0700

    growing_dto.py

--- app/application/dto/growing_dto.py
+++ app/application/dto/growing_dto.py
@@ -1,1 +1,25 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
 
+from app.domain.entities.diet_entity import DietEntity
+from app.domain.entities.factory_entity import FactoryEntity
+from app.domain.entities.production_object_entity import ProductionObjectEntity
+from app.domain.entities.production_type_entity import ProductionTypeEntity
+from app.domain.entities.shift_entity import ShiftEntity
+from app.domain.entities.user_entity import UserEntity
+
+
+@dataclass(frozen=True)
+class GrowingDTO:
+    id: Optional[int] = None
+    date_produced: Optional[datetime] = None
+    shift: Optional[ShiftEntity] = None
+    production_object: Optional[ProductionObjectEntity] = None
+    production_type: Optional[ProductionTypeEntity] = None
+    diet: Optional[DietEntity] = None
+    factory: Optional[FactoryEntity] = None
+    number_crates: Optional[int] = None
+    substrate_moisture: Optional[float] = None
+    notes: Optional[str] = None
+    user: Optional[UserEntity] = None
